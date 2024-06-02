5de39a5cd86836a9a6b96162baa7232f97696257
Otavio Brito <otaviofbrito@gmail.com>
2024-06-02 13:36:21 -0300

    crawl.py: setup argparse

--- app/view/view.py
+++ app/view/view.py
@@ -11,6 +11,28 @@ class View:
         self.root.title("Transfermarkt Scraper")
         self.root.geometry(WINDOW_SIZE)
 
+
+        self.menu_bar = tk.Menu(root)
+
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.file_menu.add_command(label="New", command=self.new_file)
+        self.file_menu.add_command(label="Open", command=self.open_file)
+        self.file_menu.add_command(label="Save", command=self.save_file)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=self.exit_app)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+
+        # Add the Edit menu
+        self.edit_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.edit_menu.add_command(label="Undo", command=self.undo_action)
+        self.edit_menu.add_command(label="Redo", command=self.redo_action)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+
+        # Display the menu
+        self.root.config(menu=self.menu_bar)
+
+
+
         self.start_button = tk.Button(
             root, text="Start MySQL", command=self.start_mysql)
         self.start_button.pack(pady=10)
@@ -21,6 +43,7 @@ class View:
         self.stop_button = tk.Button(
             root, text="Stop Scraping", command=self.stop_containers)
         self.stop_button.pack(pady=10)
+
 
 
     def start_mysql(self):
@@ -37,6 +60,26 @@ class View:
         except Exception as e:
             messagebox.showerror("Error", str(e))
 
+
+    #Menu options
+
+    def new_file(self):
+        messagebox.showinfo("New File", "New file created!")
+
+    def open_file(self):
+        messagebox.showinfo("Open File", "File opened!")
+
+    def save_file(self):
+        messagebox.showinfo("Save File", "File saved!")
+
+    def exit_app(self):
+        self.root.quit()
+
+    def undo_action(self):
+        messagebox.showinfo("Undo", "Undo action!")
+
+    def redo_action(self):
+        messagebox.showinfo("Redo", "Redo action!")
 
 if __name__ == "__main__":
     root = tk.Tk()
