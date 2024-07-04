1d4433febdc2cf128be491e55dd5210bd8c26fca
Otavio F <otaviofbrito@gmail.com>
2024-07-04 18:54:24 -0300

    feat: neo4j_club_pipeline

---
+++ tfmkt_scraper/tfmkt_scraper/pipelines/club/neo4j_club_pipeline.py
@@ -0,0 +1,18 @@
+from scrapy.exceptions import DropItem
+from tfmkt_scraper.pipelines.neo4j_connection_pipeline import Neoj4jConnectionPipeline
+
+
+class Neo4jClubPipeline(Neoj4jConnectionPipeline):
+    def process_item(self, item, spider):
+        try:
+            self.driver.execute_query(
+                """             
+                  MERGE(c:Club {club_id:$club_id})                
+                  ON CREATE SET url:$url, name:$name, current_mv:$mv, current_league:$current_league
+                  ON MATCH SET url:$url, name:$name, current_mv:$mv, current_league:$current_league
+
+                """, club_id=item['id'], url=item['url'], name=item['club_name'],
+                current_league=item['current_league'], mv=item['current_mv'], database_=self.database)
+
+        except Exception as e:
+            raise DropItem(f'**Neo4j error when inserting item: {e}')
