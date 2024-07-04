142a78c85a7813cb1df3fdfdf1c74fe8195246e4
Otavio F <otaviofbrito@gmail.com>
2024-07-04 16:18:23 -0300

    setup for neo4j pipeline

---
+++ tfmkt_scraper/tfmkt_scraper/pipelines/neo4j_connection_pipeline.py
@@ -0,0 +1,40 @@
+from neo4j import GraphDatabase
+
+
+class Neoj4jConnectionPipeline:
+    def __init__(self, uri, user, password, database) -> None:
+        self.uri = uri
+        self.user = user
+        self.password = password
+        self.database = database
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            uri=crawler.settings.get('NEO4J_URI'),
+            user=crawler.settings.get('NEO4J_USER'),
+            password=crawler.settings.get('NEO4J_PASSWORD'),
+            database=crawler.settings.get('NEO4J_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        self.driver = GraphDatabase.driver(
+            self.uri, auth=(self.user, self.password))
+        try:
+            self.driver.execute_query(
+                "CREATE CONSTRAINT player_id IF NOT EXISTS FOR (p:Player) REQUIRE p.player_id IS UNIQUE", database_=self.database)
+
+            self.driver.execute_query(
+                "CREATE CONSTRAINT league_id IF NOT EXISTS FOR (l:League) REQUIRE l.league_id IS UNIQUE", database_=self.database)
+
+            self.driver.execute_query(
+                "CREATE CONSTRAINT club_id IF NOT EXISTS FOR (c:Club) REQUIRE c.club_id IS UNIQUE", database_=self.database)
+
+            self.driver.execute_query(
+                "CREATE CONSTRAINT transfer IF NOT EXISTS FOR (t:Transfer) REQUIRE (t.player_id, t.clubLeft_id, t.clubJoined_id, t.year) IS UNIQUE", database_=self.database)
+
+        except Exception as e:
+            print(e)
+
+    def close_spider(self, spider):
+        self.driver.close()
--- tfmkt_scraper/tfmkt_scraper/settings.py
+++ tfmkt_scraper/tfmkt_scraper/settings.py
@@ -23,6 +23,12 @@ MYSQL_USER = "user"
 MYSQL_PASSWORD = "user"
 MYSQL_DATABASE = "tm_db"
 
+# Neo4j SETTINGS
+NEO4J_URI = "neo4j://localhost:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "secretgraph"
+NEO4J_DATABASE = "neo4j"
+
 #HTTP CACHE SETTINGS
 DOWNLOADER_MIDDLEWARES = {
     "scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware": 500
