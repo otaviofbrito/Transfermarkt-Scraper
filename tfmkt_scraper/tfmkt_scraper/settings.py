201591c7dcf82cb9a2d50cfcf58151ec0d62f251
Otavio F <otaviofbrito@gmail.com>
2024-07-05 14:03:49 -0300

    fix: set neo4j container hostname to the uri

--- tfmkt_scraper/tfmkt_scraper/settings.py
+++ tfmkt_scraper/tfmkt_scraper/settings.py
@@ -24,12 +24,12 @@ MYSQL_PASSWORD = "user"
 MYSQL_DATABASE = "tm_db"
 
 # Neo4j SETTINGS
-NEO4J_URI = "neo4j://localhost:7687"
+NEO4J_URI = "neo4j://neo4j:7687"
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "secretgraph"
 NEO4J_DATABASE = "neo4j"
 
-#HTTP CACHE SETTINGS
+# HTTP CACHE SETTINGS
 DOWNLOADER_MIDDLEWARES = {
     "scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware": 500
 }
