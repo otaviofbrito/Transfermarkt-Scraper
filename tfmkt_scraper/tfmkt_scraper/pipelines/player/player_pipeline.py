f6b89e86bd3d90303416d1d01b29617c7b444b85
Otavio F <otaviofbrito@gmail.com>
2024-07-07 01:39:15 -0300

    refactor player_pipeline and fix empty string key value

--- tfmkt_scraper/tfmkt_scraper/pipelines/player/player_pipeline.py
+++ tfmkt_scraper/tfmkt_scraper/pipelines/player/player_pipeline.py
@@ -16,55 +16,53 @@ class PlayerScraperPipeline:
                 adapter[field] = value.strip()
 
         # Convert market value
-        mv_keys = ['current_mv']
-        for mv_key in mv_keys:
-            value = adapter.get(mv_key)
-            if value:
-                adapter[mv_key] = convert_mv(value=value)
-            else:
-                adapter[mv_key] = 0
+        value = adapter.get('current_mv')
+        if value:
+            adapter['current_mv'] = convert_mv(value=value)
+        else:
+            adapter['current_mv'] = 0
 
         # Convert club ID to int
         id_keys = ['id', 'current_club']
         convert_item_str_to_int(adapter=adapter, keys=id_keys)
 
         # Convert height to cm
-        height_keys = ['height']
-        for height_key in height_keys:
-            value = adapter.get(height_key)
-            if value:
-                value = value.replace('m', '')
-                value = value.replace(',', '')
-                value = int(value)
-                adapter[height_key] = value
+        value = adapter.get('height')
+        if value:
+            value = value.replace('m', '')
+            value = value.replace(',', '')
+            value = int(value)
+            adapter['height'] = value
 
         # Convert birth_date
         # remove age
-        date_keys = ['birth_date']
-        for date_key in date_keys:
-            date = adapter.get(date_key)
-            if date:
-                date = re.sub(r'\([^()]*\)', '', date).strip()
-                try:
-                    date = datetime.datetime.strptime(
-                        date, '%b %d, %Y').strftime('%Y-%m-%d')
-                    adapter[date_key] = date
-                except ValueError:
-                    # If date only contains year, set it to None
-                    adapter[date_key] = None
+        date = adapter.get('birth_date')
+        if date:
+            date = re.sub(r'\([^()]*\)', '', date).strip()
+            try:
+                date = datetime.datetime.strptime(
+                    date, '%b %d, %Y').strftime('%Y-%m-%d')
+                adapter['birth_date'] = date
+            except ValueError:
+                # If date only contains year, set it to None
+                adapter['birth_date'] = None
 
         # Convert death_date
-        date_keys = ['death_date']
-        for date_key in date_keys:
-            date = adapter.get(date_key)
-            if date:
-                date = re.sub(r'\([^()]*\)', '', date).strip()
-                try:
-                    date = datetime.datetime.strptime(
-                        date, '%d.%m.%Y').strftime('%Y-%m-%d')
-                    adapter[date_key] = date
-                except ValueError:
-                    # If date only contains year, set it to None
-                    adapter[date_key] = None
+        date = adapter.get('death_date')
+        if date:
+            date = re.sub(r'\([^()]*\)', '', date).strip()
+            try:
+                date = datetime.datetime.strptime(
+                    date, '%d.%m.%Y').strftime('%Y-%m-%d')
+                adapter['death_date'] = date
+            except ValueError:
+                # If date only contains year, set it to None
+                adapter['death_date'] = None
+
+        # Convert empty strings to None | '' -> None
+        for field in field_names:
+            value = adapter.get(field)
+            if value == '':
+                adapter[field] = None
 
         return item
