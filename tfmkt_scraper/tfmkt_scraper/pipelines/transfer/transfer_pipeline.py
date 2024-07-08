e705d49031b95ff724d629da6c82aa47e80eb7a1
Otavio <otaviofbrito@gmail.com>
2024-07-07 23:28:15 -0300

    refactor: treat empty strings

--- tfmkt_scraper/tfmkt_scraper/pipelines/transfer/transfer_pipeline.py
+++ tfmkt_scraper/tfmkt_scraper/pipelines/transfer/transfer_pipeline.py
@@ -1,21 +1,23 @@
 from itemadapter import ItemAdapter
-from tfmkt_scraper.utils import convert_mv, convert_item_str_to_int
+from tfmkt_scraper.utils import *
+
 
 class TransferScraperPipeline:
-  def process_item(self, item, spider):
-    
-    adapter =ItemAdapter(item)
-    
-    #Convert values to int
-    str_keys = ['joined_club_id', 'left_club_id', 'player_id', 'year']
-    convert_item_str_to_int(adapter=adapter, keys=str_keys)
-    
-    #Convert transfer fee
-    fee_keys = ['transfer_fee']
-    for key in fee_keys:
-      value = adapter.get(key)
-      if isinstance(value, str):
-         value = value.replace('€', '')
-         adapter[key] = convert_mv(value)
-    
-    return item
+    def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        # Convert values to int
+        str_keys = ['joined_club_id', 'left_club_id', 'player_id', 'year']
+        convert_item_str_to_int(adapter=adapter, keys=str_keys)
+
+        # Convert transfer fee
+        key = 'transfer_fee'
+        value = adapter.get(key)
+        if isinstance(value, str):
+            value = value.replace('€', '')
+            adapter[key] = convert_mv(value)
+
+        convert_emptystring_to_none(adapter=adapter)
+
+        return item
