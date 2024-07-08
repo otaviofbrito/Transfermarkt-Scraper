d5ddaba908894af103d0899bcc7657456f0569ab
Otavio <otaviofbrito@gmail.com>
2024-07-08 15:06:04 -0300

    fix: cypher query -  add player_id as Transfer property

--- tfmkt_scraper/tfmkt_scraper/pipelines/transfer/neo4j_transfer_pipeline.py
+++ tfmkt_scraper/tfmkt_scraper/pipelines/transfer/neo4j_transfer_pipeline.py
@@ -6,19 +6,21 @@ class Neo4jTransferPipeline(Neoj4jConnectionPipeline):
     def process_item(self, item, spider):
         try:
             self.driver.execute_query(
-                """             
-                  MERGE(p:Player {player_id:$player_id}) 
-                  MERGE(cl:Club {club_left_id:$club_left_id})    
-                  MERGE(c2:Club {club_joined_id:$club_joined_id})    
+                """
+                  MERGE(p:Player {player_id:$player_id})
+                  MERGE(c1:Club {club_id:$club_left_id})
+                  MERGE(c2:Club {club_id:$club_joined_id})
 
-                  MERGE(t:Transfer)
-                  ON CREATE SET t.year=$year, t.fee=$fee, t.type=$type
-                  ON MATCH SET t.year=$year, t.fee=$fee, t.type=$type
-
-                  MERGE(c1)<-[:LEFT]-(t)-[:JOINED]->(c2)
+                  MERGE(c1)<-[:LEFT]-(t:Transfer {year:$year, fee:$fee, type:$type, player_id:$player_id})-[:JOINED]->(c2)
                   MERGE(t)-[:OF_PLAYER]->(p)
-                """, club_id=item['id'], url=item['url'], name=item['club_name'],
-                current_league=item['current_league'], mv=item['current_mv'], database_=self.database)
-
+                """,
+                player_id=item['player_id'],
+                year=item['year'],
+                club_left_id=item['left_club_id'],
+                club_joined_id=item['joined_club_id'],
+                fee=item['transfer_fee'],
+                type=item['transfer_type'],
+                database_=self.database)
+            return item
         except Exception as e:
             raise DropItem(f'**Neo4j error when inserting item: {e}')
